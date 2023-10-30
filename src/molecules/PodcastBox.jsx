a564cf278b2b290095c6fbd7e662d8b612df000d
Jessica Ríos Maneiro <117263066+JessRm04@users.noreply.github.com>
2023-10-30 22:18:59 +0100

    Add PodcastBox main component, configure fetch API

---
+++ src/molecules/PodcastBox.jsx
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from 'react';
+import './PodcastBox.css'
+import PodcastCard from '../atoms/PodcastCard';
+
+const PodcastBox = () => {
+  const [podcasts, setPodcasts] = useState([]);
+
+  useEffect(() => {
+    const getPodcast = async () => {
+      try {
+        const response = await fetch('https://itunes.apple.com/us/rss/toppodcasts/limit=100/genre=1310/json');
+        if (response.ok) {
+          const data = await response.json();
+          setPodcasts(data.feed.entry); // Almacenar los podcasts en el estado
+        } else {
+          throw new Error('Error al obtener los podcasts populares');
+        }
+      } catch (error) {
+        console.error('Error:', error);
+      }
+    };
+
+    getPodcast();
+  }, []);
+
+  return (
+    <div>
+      {podcasts.map((podcast) => (
+        <PodcastCard
+          key={podcast.id.label} 
+          image={podcast['im:image'][0].label} 
+          name={podcast['im:name'].label} 
+          artist={podcast['im:artist'].label} 
+        />
+      ))}
+    </div>
+  );
+};
+
+export default PodcastBox;
