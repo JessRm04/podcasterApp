758bea74afb01a8c3e303d975be7ce3416c9bab0
Jessica Ríos Maneiro <117263066+JessRm04@users.noreply.github.com>
2023-11-05 10:08:55 +0100

    Add Context.provider to Router

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,17 +1,24 @@
+import React, { useState } from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainPage from "./pages/MainPage.jsx";
 import PodcastDetailPage from "./pages/PodcastDetailPage.jsx";
+import EpisodeDetailPage from "./pages/EpisodeDetailPage.jsx";
+import { SelectedEpisodeContext } from '../src/Context.jsx'
+
 const Router = () => {
+  const [selectedEpisode, setSelectedEpisode] = useState(null);
 
   return (
-    <>
+    <SelectedEpisodeContext.Provider value={{ selectedEpisode, setSelectedEpisode }}>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<MainPage />}></Route>
           <Route path="podcast/:podcastId" element={<PodcastDetailPage />}></Route>
+          <Route path="podcast/:podcastId/episode/:trackId" element={<EpisodeDetailPage />}></Route>
         </Routes>
       </BrowserRouter>
-    </>
+    </SelectedEpisodeContext.Provider>
   );
 };
+
 export default Router;
